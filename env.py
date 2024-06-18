032dfb717c57f7624bd6e966df394a4f4c72cf01
Rehan Ahmad Rai <rehan.ahmad.rai@gmail.com>
2024-06-18 16:47:49 +0000

    connected to data base

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ.setdefault("DATABASE_URL", "postgres://snaohnbc:uKgNK35YgpnnHQiitU-ViFFHZUAvoY0x@manny.db.elephantsql.com/snaohnbc")
+os.environ.setdefault("SECRET_KEY", "django-insecure-!#onyi_l*2oams#7tzt-1r09-aebw5dzk5$xn10-hyndh^q(_2")
