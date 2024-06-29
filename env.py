d4239b746d3b423da0778869a4b4c5c7dd4a15db
Rehan Ahmad Rai <rehan.ahmad.rai@gmail.com>
2024-06-29 12:10:44 +0000

    remove Env file

--- env.py
+++
@@ -1,4 +0,0 @@
-import os
-
-os.environ.setdefault("DATABASE_URL", "postgres://snaohnbc:uKgNK35YgpnnHQiitU-ViFFHZUAvoY0x@manny.db.elephantsql.com/snaohnbc")
-os.environ.setdefault("SECRET_KEY", "django-insecure-!#onyi_l*2oams#7tzt-1r09-aebw5dzk5$xn10-hyndh^q(_2")
