155a86e65e5eeb797d95c308b1c7428f7aaabe68
Rehan Ahmad Rai <rehan.ahmad.rai@gmail.com>
2024-06-29 20:33:30 +0000

    remove security key from settings py

--- e_shop/settings.py
+++ e_shop/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!#onyi_l*2oams#7tzt-1r09-aebw5dzk5$xn10-hyndh^q(_2'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
