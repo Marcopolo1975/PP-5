ccbd9186776db43f20cb73e5445482cce2248db1
Rehan Ahmad Rai <rehan.ahmad.rai@gmail.com>
2024-07-06 20:38:08 +0000

    added product delete confirmation function

--- products/views.py
+++ products/views.py
@@ -1,18 +1,29 @@
+"""Product Views"""
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib.auth.decorators import (
+    login_required, user_passes_test, PermissionDenied)
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 from django.db.models import Q
 from django.db.models.functions import Lower
 
 from .models import Product, Category
 from .forms import ProductForm
 
-# Create your views here.
+
+def superuser_check(user):
+    """ checks if user if superuser """
+    if user.is_superuser:
+        return True
+    raise PermissionDenied
+
 
 def all_products(request):
-    """ A view to show all products, including sorting and search queries """
+    """ A view to show all products, sorting and search queries """
 
-    products = Product.objects.all()
+    products = Product.objects.all().order_by('sku')
     query = None
     categories = None
     sort = None
@@ -25,14 +36,13 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-            if sortkey == 'category':
-                sortkey = 'category__name'
+
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-            
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -41,10 +51,12 @@ def all_products(request):
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You didn't enter any search criteria!")
+                messages.error(request, "You didn't enter any search critera")
                 return redirect(reverse('products'))
-            
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
+
+            queries = Q(
+                name__icontains=query) | Q(description__icontains=query) | Q(
+                excerpt__icontains=query)
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
@@ -72,12 +84,9 @@ def product_detail(request, product_id):
 
 
 @login_required
+@user_passes_test(superuser_check)
 def add_product(request):
     """ Add a product to the store """
-    if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only store owners can do that.')
-        return redirect(reverse('home'))
-
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
@@ -85,10 +94,12 @@ def add_product(request):
             messages.success(request, 'Successfully added product!')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+            messages.error(
+                request, """ Failed to add product. Please ensure the
+                form is valid.""")
     else:
         form = ProductForm()
-        
+
     template = 'products/add_product.html'
     context = {
         'form': form,
@@ -98,12 +109,9 @@ def add_product(request):
 
 
 @login_required
+@user_passes_test(superuser_check)
 def edit_product(request, product_id):
     """ Edit a product in the store """
-    if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only store owners can do that.')
-        return redirect(reverse('home'))
-
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
@@ -112,7 +120,8 @@ def edit_product(request, product_id):
             messages.success(request, 'Successfully updated product!')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+            messages.error(request, """ Failed to update product. Please ensure
+            the form is valid. """)
     else:
         form = ProductForm(instance=product)
         messages.info(request, f'You are editing {product.name}')
@@ -126,14 +135,29 @@ def edit_product(request, product_id):
     return render(request, template, context)
 
 
-@login_required
-def delete_product(request, product_id):
-    """ Delete a product from the store """
-    if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only store owners can do that.')
-        return redirect(reverse('home'))
+class DeleteProduct(
+        LoginRequiredMixin, UserPassesTestMixin, generic.DeleteView):
+    """
+    This view is used to allow the superuser to delete a service
+    """
+    model = Product
+    template_name = 'products/delete_product.html'
+    success_message = "Product deleted successfully"
+    success_url = reverse_lazy('products')
 
-    product = get_object_or_404(Product, pk=product_id)
-    product.delete()
-    messages.success(request, 'Product deleted!')
-    return redirect(reverse('products'))
+    def test_func(self):
+        """
+       Ensure only superuser can edit service details
+        """
+        if self.request.user.is_superuser:
+            return True
+
+    def delete(self, request, *args, **kwargs):
+        """
+        This function is used to display success message given
+        SucessMessageMixin cannot be used in generic.DeleteView.
+        Credit: https://stackoverflow.com/questions/24822509/
+        success-message-in-deleteview-not-shown
+        """
+        messages.success(self.request, self.success_message)
+        return super(DeleteProduct, self).delete(request, *args, **kwargs)
