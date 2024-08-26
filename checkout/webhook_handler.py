8f71316c076af089b7e855f2310b86e055ff2eed
Rehan Ahmad Rai <rehan.ahmad.rai@gmail.com>
2024-08-26 11:12:35 +0000

    update stripe charge statement in webhook-handler.py

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -52,9 +52,18 @@ class StripeWH_Handler:
         bag = intent.metadata.bag
         save_info = intent.metadata.save_info
 
+       
+
+    
+
     #    billing_details = intent.charges.data[0].billing_details
     #    shipping_details = intent.shipping
     #    grand_total = round(intent.charges.data[0].amount / 100, 2)
+    
+    # Get the Charge object
+        stripe_charge = stripe.Charge.retrieve(
+        intent.latest_charge
+             )
 
         billing_details = stripe_charge.billing_details # updated
         shipping_details = intent.shipping
